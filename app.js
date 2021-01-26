2806129ea670ad45e81c4f15aca320bd18936d41
GayatriPachkawde <gayatri5kawde@gmail.com>
2021-01-26 19:40:39 +0530

    commit all changes

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + "/client/build"));
+app.use(express.static(__dirname + "/build"));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 module.exports = app;
